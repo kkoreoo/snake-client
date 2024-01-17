5efd4d6b4b402419237d91aaa5f92db4c8ef841e
atle <alextle24@gmail.com>
2024-01-17 14:28:40 -0700

    modulated play.js by putting input function into a seperate file

---
+++ input.js
@@ -0,0 +1,20 @@
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  stdin.on('data', handleUserInput);
+
+  const handleUserInput = (data) => {
+    if (data === '\u0003') {
+      process.exit();
+    }
+  }
+
+  return stdin;
+};
+
+module.exports = {
+  setupInput,
+}
--- play.js
+++ play.js
@@ -1,23 +1,8 @@
 const net = require("net");
 // establishes a connection with the game server
 const { connect } = require('./client');
+const { setupInput } = require('./input');
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-
-  stdin.on('data', handleUserInput);
-
-  const handleUserInput = (data) => {
-    if (data === '\u0003') {
-      process.exit();
-    }
-  }
-
-  return stdin;
-};
 
 console.log("Connecting ...");
 connect();
